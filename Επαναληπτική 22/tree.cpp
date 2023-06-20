aa67221b5b635f1549c2db791e72d8362490d8f9
NikosBougatsas <127541509+NikosBougatsas@users.noreply.github.com>
2023-06-20 17:58:20 +0300

    Create tree.cpp

---
+++ Επαναληπτική 22/tree.cpp
@@ -0,0 +1,26 @@
+//Θέμα 3 επαναληπτική 22
+struct node {
+  int data;
+  node *left, *right;
+};
+
+bool func (node *root, const int &k) {
+  if (root == NULL) return false;
+  bool flag = false;
+  f_help(root, k, flag, root->data);
+  return flag;
+}
+
+void f_help (node *n, const int &k, bool &f, int &count) {
+  if (count == k) f = true; return;
+  else if (count > k) return;
+  else {
+    if (n->left != NULL) {
+      f_help(n->left, k, f, count + (n->left)->data);
+    }
+    if (n->right != NULL) {
+      f_help(n->right, k, f, count + (n->right)->data);
+    }
+    return;
+  }
+}
