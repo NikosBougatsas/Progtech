5c6615a81f577b096642ffe98ab0d8df02c36be6
NikosBougatsas <127541509+NikosBougatsas@users.noreply.github.com>
2023-07-18 15:14:39 +0300

    Create tree.cpp

---
+++ Κανονική 23/tree.cpp
@@ -0,0 +1,16 @@
+template<typename T>
+
+struct node {
+  T data;
+  node *left, *right;
+};
+
+int freq (node *t, T &x, int h) {
+  if (t == NULL) return 0;
+  if (h == 1){
+    if (t->data == x) return 1;
+    else return 0;
+  }
+  else if (h > 1)
+    return freq(t->left, x, h-1) + freq(t->right, x, h-1);
+}
